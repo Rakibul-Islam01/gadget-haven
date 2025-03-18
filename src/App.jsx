4d34961ee4996229136173a2f6bd8c06f83f7e06
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2025-03-18 12:52:41 +0600

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button
+        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='bg-[#9538E2]'>
+            <h3 className='text-5xl font-bold text-center pt-12 pb-6 text-white leading-normal'>Upgrade Your Tech Accessorize with <br /> Gadget Heaven Accessories</h3>
+            <p>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+            <div className='flex justify-center'>
+                <div className="btn ">Shop Now</div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
